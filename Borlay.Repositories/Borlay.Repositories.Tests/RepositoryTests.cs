335a4e711542b5c1e43a62c3030859d843379994
Paulius Kanoverskis <P.Kanoverskis@post.lt>
2019-04-05 14:15:32 +0300

    test project

---
+++ Borlay.Repositories/Borlay.Repositories.Tests/RepositoryTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace Borlay.Repositories.Tests
+{
+    [TestClass]
+    public class RepositoryTests
+    {
+        [TestMethod]
+        public void UnitTest()
+        {
+
+        }
+    }
+}
